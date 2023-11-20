3828e9df4fc6c1c4e845770777e9767deec502ab
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-11-20 19:55:47 +0200

    create the teplate of the project

---
+++ SQL & PostgreSQL/restdbapp/src/main/java/com/academy/sirma/restdbapp/RestDbAppApplication.java
@@ -0,0 +1,13 @@
+package com.academy.sirma.restdbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDbAppApplication.class, args);
+	}
+
+}
---
+++ SQL & PostgreSQL/restdbapp/src/test/java/com/academy/sirma/restdbapp/RestDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.sirma.restdbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
