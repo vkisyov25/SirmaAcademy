a1975ff2910fe429e111a40e5881f609134357af
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-11-20 20:06:47 +0200

    create the models

---
+++ SQL & PostgreSQL/restdbapp/src/main/java/com/academy/sirma/restdbapp/Model/Article.java
@@ -0,0 +1,47 @@
+package com.academy.sirma.restdbapp.Model;
+
+public class Article {
+    private int id;
+    private String title;
+    private String content;
+    private String userId;
+
+    public Article(int id, String title, String content, String userId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
