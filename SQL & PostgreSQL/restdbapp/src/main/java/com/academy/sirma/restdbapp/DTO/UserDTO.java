329812f6be3e2ab9daa46ff671a8047700e96858
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-11-22 19:10:06 +0200

    create UserDTO

---
+++ SQL & PostgreSQL/restdbapp/src/main/java/com/academy/sirma/restdbapp/DTO/UserDTO.java
@@ -0,0 +1,34 @@
+package com.academy.sirma.restdbapp.DTO;
+
+public class UserDTO {
+    private int id;
+    private String username;
+    private String email;
+    public UserDTO() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
