5e9299e53b30bc303d02e19938078050c1f5c7cf
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-11-22 19:12:01 +0200

    created UserMapper

---
+++ SQL & PostgreSQL/restdbapp/src/main/java/com/academy/sirma/restdbapp/DTO/UserMapper.java
@@ -0,0 +1,13 @@
+package com.academy.sirma.restdbapp.DTO;
+
+import com.academy.sirma.restdbapp.Model.User;
+
+public class UserMapper {
+
+    public static  UserDTO toUserDto(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId()); 
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+    }
+}
